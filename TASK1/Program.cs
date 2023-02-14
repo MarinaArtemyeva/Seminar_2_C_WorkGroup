d07a865ada65fe3cb178ec2ea27aabffdac92b6e
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-14 16:05:48 +0600

    Add code in TASK1

--- TASK1/Program.cs
+++ TASK1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая выводит случайное 
+//число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//Например:
+//78 -> 8
+//12-> 2
+//85 -> 8
+
+int a = new Random().Next(10,99);
+int b = a / 10;
+int c = a % 10;
+Console.WriteLine(a);
+if (c > b)
+{
+    Console.WriteLine(c);
+}
+else 
+{
+    Console.WriteLine(b);
+}
