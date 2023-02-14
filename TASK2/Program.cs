5bb2c5b3752dce3549f1d37a048b405a957782e4
Marina Artemyeva <martemyeva@yahoo.com>
2023-02-14 16:11:04 +0600

    Add code in TASK2

--- TASK2/Program.cs
+++ TASK2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное 
+// трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int value = int.Parse(Console.ReadLine());
+int a = value / 100;
+int c = value % 10;
+
+int result = a * 10 + c;
+Console.WriteLine(result);
